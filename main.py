a581182ed8f5e1c9d0d93340fb6b35cb9b269aa2
sbahmann <bahamonsebastian@hotmail.com>
2021-01-21 18:21:51 +0100

    todo el codigo

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-a=int(input("primer numero "))
-b=int(input("segundo numero "))
-c=a+b
-print ("resultado",5c)
+import openpyxl
+book = openpyxl.load_workbook('C:\\Users\\2499311\\Desktop\\datosphyton.xlsx')
+tabelle1 = book.active
+celdas = tabelle1['A1':'D4']
+lista_empleados = []
+factor=3
+for fila in celdas:
+    empleado = [celda.value for celda in fila]
+    lista_empleados.append(empleado)
+for empleado in lista_empleados:
+    print(f'don {empleado[0]} es el {empleado[1]} y gana {empleado[2]*factor}')
+
+from matplot import pyplot
